8edf8f552c5e890202ac7330cd458f25c810f311
Justin Steffen <JustinDSN@gmail.com>
2014-02-22 19:55:29 -0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Justin Steffen on 2/22/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
