14290fabf97538b36d1eeeb6a2712ce92b8ffdce
Justin Steffen <JustinDSN@gmail.com>
2014-02-22 21:54:42 -0800

    Completing Chapter 2

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,15 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *items = [[NSMutableArray alloc] init];
         
+        for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
+        
+        //Bronze Challenge
+        //BNRItem *eleventhItem = items[11];
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        items = nil;
     }
     return 0;
 }
